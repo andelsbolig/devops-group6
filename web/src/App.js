f3647f868311a3ce2b64851a8de9f8c6c6a463d5
JonathanKN <62690796+JonathanKN@users.noreply.github.com>
2025-09-18 13:48:54 +0200

    Add login page

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+const Home = () => {
+  return <div>
+      Home Page
+      <Link to={"/login"}>Go to login</Link>
+  </div>;
+}
+
+const Login = () => {
+  return <div>Login Page</div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button>Login</button>
-      </header>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
     </div>
   );
 }
